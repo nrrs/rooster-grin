db6cf6432dc10f3f2c7c7ca9291ba7d04b0622bd
Norris Kwan <norris.kwan@gmail.com>
2017-09-08 14:08:55 -0700

    Add public

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     entry: './src/app.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
+        publicPath: "/public/",
         filename: 'bundle.js'
     },
 
