1c0608f820e8044207cc05d1f145d0ce899ec83d
Norris Kwan <norris.kwan@gmail.com>
2017-09-08 13:15:28 -0700

    Remove script

--- src/app.js
+++ src/app.js
@@ -3,5 +3,3 @@ import Styles from './styles/main.css';
 import SkelSettings from './js/skel-settings.js';
 import SlickSettings from './js/slick-settings.js';
 import Script from './js/script.js';
-
-console.log("butters");
