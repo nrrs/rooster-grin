2f0be42b2d092a3972c12770571801103cc0130b
Norris Kwan <norris.kwan@gmail.com>
2017-09-07 23:27:13 -0700

    Add animations, refactor, style

--- src/app.js
+++ src/app.js
@@ -1,55 +1,5 @@
 import Home from '../index.html';
 import Styles from './styles/main.css';
-
-console.log("working");
-
-skel.breakpoints({
-    xlarge: "(max-width: 1680px)",
-    large: "(max-width: 1280px)",
-    medium: "(max-width: 980px)",
-    small: "(max-width: 736px)",
-    xsmall: "(max-width: 480px)"
-});
-
-skel.layout({
-    reset: "normalize",
-    containers: true,
-    grid: true,
-    breakpoints: {
-        medium: {
-            containers: "90%"
-        },
-        small: {
-            containers: "95%",
-            grid: { gutters: 20 }
-        },
-        xsmall: {
-            grid: { gutters: 10 }
-        }
-    }
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-    // Fade Checkers on load
-    $('#checkers').animate({ 'opacity': '1' }, 1500);
-    // On scroll
-    $(window).scroll(function() {
-        $('#logo').animate({ 'background-position-x': '0%' }, 500);
-        // Select .fadeIn, calc 1/3 of view, fade it in.
-        $('.fadeIn').each(function(i) {
-            const bottom = ($(this).position().top + $(this).outerHeight() / 3);
-            const windowBottom = $(window).scrollTop() + $(window).height();
-            // If 1/3 of object is in the window, fade it in
-            if (windowBottom > (bottom)) {
-                $(this).animate({ 'opacity': '1' }, 1000);
-            }
-        });
-    });
-
-
-    // init Slick Carousel
-
-});
-
-
-console.log("hi");
+import SkelSettings from './js/skel-settings.js';
+import SlickSettings from './js/slick-settings.js';
+import Script from './js/script.js';
---
+++ src/js/script.js
@@ -0,0 +1,61 @@
+// Monkey Patch jQuery prototype
+$.fn.isInViewport = function() {
+    const elementTop = $(this).offset().top;
+    const elementBottom = elementTop + $(this).outerHeight();
+    const viewportTop = $(window).scrollTop();
+    const viewportBottom = viewportTop + $(window).height();
+
+    return (elementBottom > viewportTop && elementTop < viewportBottom);
+};
+
+
+// Animate Percents
+const animatePercent = function(id, num) {
+    let i = 0;
+    let interval = (num > 200) ? 10 : 20;
+
+    let counter = setInterval(() => {
+        document.getElementById(`${id}`).innerHTML = i;
+
+        if (i > num) {
+            document.getElementById(`${id}`).innerHTML = num;
+            clearInterval(counter);
+        }
+
+        i += 3;
+    }, interval);
+
+};
+
+let flag = true;
+
+// Doc ready
+document.addEventListener("DOMContentLoaded", () => {
+    // Fade Checkers on load
+    $('#checkers').animate({ 'opacity': '1' }, 1500);
+
+    // On scroll
+    $(window).scroll(function() {
+        $('#logo').animate({ 'background-position-x': '0%' }, 500);
+        // Select .fadeIn, calc 1/3 of view, fade it in.
+        $('.fadeIn').each(function(i) {
+            const bottom = ($(this).position().top + $(this).outerHeight() / 3);
+            const windowBottom = $(window).scrollTop() + $(window).height();
+            // If 1/3 of object is in the window, fade it in
+            if (windowBottom > (bottom)) {
+                $(this).animate({ 'opacity': '1' }, 1000);
+            }
+
+            if ($('#percents').isInViewport()) {
+                if (flag) {
+                    animatePercent('stat-one', 240);
+                    animatePercent('stat-two', 105);
+                    animatePercent('stat-three', 159);
+                    flag = false;
+                }
+            }
+
+        });
+    });
+
+});
