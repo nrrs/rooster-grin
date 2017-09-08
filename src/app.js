c918e5db05befb5c5b7f1e75dea65202cdd9cf34
Norris Kwan <norris.kwan@gmail.com>
2017-09-08 12:54:42 -0700

    Update bundle

--- src/app.js
+++ src/app.js
@@ -3,3 +3,5 @@ import Styles from './styles/main.css';
 import SkelSettings from './js/skel-settings.js';
 import SlickSettings from './js/slick-settings.js';
 import Script from './js/script.js';
+
+console.log("butters");
